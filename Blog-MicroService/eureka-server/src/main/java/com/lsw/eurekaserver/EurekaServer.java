9d2a5eb10d02cceee6c82330ad329eeb68fa53e5
734652785@qq.com <linxing369>
2020-05-19 13:13:23 +0800

    123

---
+++ Blog-MicroService/eureka-server/src/main/java/com/lsw/eurekaserver/EurekaServer.java
@@ -0,0 +1,19 @@
+package com.lsw.eurekaserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+/**
+ * @Desc TODO
+ * @Author LinXing
+ * @Date 2020/5/19 13:11
+ * @Version 1.0
+ */
+@SpringBootApplication
+@EnableEurekaServer
+public class EurekaServer {
+    public static void main(String[] args) {
+        SpringApplication.run(EurekaServer.class,args);
+    }
+}
